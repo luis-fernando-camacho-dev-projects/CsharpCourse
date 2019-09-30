e01d392ca588c5f1f9502cbe3192a153590e1fdf
Luis Fernando Camacho <Luis.camacho@topssoft.com>
2019-09-30 13:55:14 -0400

    while example

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChooseAnswerCorrect();
+        }
+
+
+        public static void ChooseAnswerCorrect()
+        {
+            Console.WriteLine("What is the keyboard in c# to finish a loop(while,for)");
+            char answer = 'a';
+            Console.WriteLine("a. int");
+            Console.WriteLine("b. continue");
+            Console.WriteLine("c. break");
+            Console.WriteLine("d. exit");
+            while ( answer != 'c')
+            {
+                Console.WriteLine("Enter choice:");
+                answer = Console.ReadLine().First();
+                if (answer != 'c')
+                {
+                    Console.WriteLine("Incorrect Answer");
+                }
+                else
+                {
+                    Console.WriteLine("Correct Answer");
+                }
+            }
+            Console.WriteLine("Finish Program");
+        }
+    }
+}
